b23f1cd554044be98f0434d231b574e22c5c8cd0
alex_stivi <alexstividor@gmail.com>
2024-03-14 23:03:36 +0200

    added scrolling employees list

--- app/src/main/java/com/example/alex_shirkovets_kode_trainee_2024/MainActivity.kt
+++ app/src/main/java/com/example/alex_shirkovets_kode_trainee_2024/MainActivity.kt
@@ -3,21 +3,30 @@ package com.example.alex_shirkovets_kode_trainee_2024
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.alex_shirkovets_kode_trainee_2024.ui.theme.Alexshirkovetskodetrainee2024Theme
 
 class MainActivity : ComponentActivity() {
@@ -37,8 +46,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -51,26 +58,41 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun StartScreen() {
     Column {
 
-        Spacer(modifier = Modifier.padding(all=20.dp))
+        Spacer(modifier = Modifier.padding(all = 20.dp))
         TopAppBar()
-        Greeting("Android")
+        Greeting("Designers")
+        EmployeesList(EmployeesNamesSampleData.employees )
     }
 }
 
 @Composable
 fun ContentTab(names: List<DepartmentName>) {
     LazyRow {
-        items(names) {name ->
+        items(names) { name ->
             DepartmentNameTab(name)
         }
     }
 }
 
+
+@Composable
+fun EmployeesList(names: List<EmployeeInfo>) {
+    LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
+        items(names) { name ->
+            EmployeesShortInfo(name)
+        }
+    }
+}
+
+
+
+
+
 @Composable
 fun TopAppBar() {
     Column {
         Text("TOP")
-        Text("MIDDLE")
+        Text("ПОИСК")
         ContentTab(DepartmentsNamesSampleData.depNamesSample)
     }
 }
@@ -79,12 +101,84 @@ data class DepartmentName(val name: String)
 
 @Composable
 fun DepartmentNameTab(tab: DepartmentName) {
-    Text(text = tab.name, Modifier
-        //.clickable(onClick = onClick)
-        .padding(horizontal = 12.dp, vertical = 8.dp)
+    Text(
+        text = tab.name, Modifier
+            //.clickable(onClick = onClick)
+            .padding(horizontal = 12.dp, vertical = 8.dp)
     )
 }
 
+data class EmployeeInfo(
+    val id: String,
+    val avatarUrl: String,
+    val firstName: String,
+    val lastName: String,
+    val userTag: String,
+    val department: String,
+    val position: String,
+    val birthday: String,
+    val phone: String
+)
+
+@Composable
+fun EmployeesShortInfo(employee: EmployeeInfo) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+
+        AsyncImage(
+            model = employee.avatarUrl,
+            contentDescription = null,
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(72.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+
+
+
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+
+            Row {
+                Text(
+                    text = employee.firstName,
+                    modifier = Modifier.padding(all = 4.dp),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+
+                Text(
+                    text = employee.lastName,
+                    modifier = Modifier.padding(all = 4.dp),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+
+                Text(
+                    text = employee.userTag,
+                    modifier = Modifier.padding(all = 4.dp),
+                    color = MaterialTheme.colorScheme.onSecondary,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            }
+
+            Text(
+                text = employee.position,
+                modifier = Modifier.padding(all = 4.dp),
+                color = MaterialTheme.colorScheme.onTertiary,
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/alex_shirkovets_kode_trainee_2024/ui/theme/Color.kt
+++ app/src/main/java/com/example/alex_shirkovets_kode_trainee_2024/ui/theme/Color.kt
@@ -2,6 +2,10 @@ package com.example.alex_shirkovets_kode_trainee_2024.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val MiddleTitle = Color(0xFF050510)
+val MiddleMeta = Color(0xFF97979B)
+val SubTitle = Color(0xFF55555C)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +13,5 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
